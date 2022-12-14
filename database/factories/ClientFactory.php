7fd87e13ac610b1f1276de8c5a30ca7460f15b8c
Erik Masny <erik.masny@gmail.com>
2022-12-14 23:39:43 +0100

    feat: create db backend structure for clients

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
+ */
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company(),
+            'hour_rate' => $this->faker->numberBetween(10, 40),
+        ];
+    }
+}
